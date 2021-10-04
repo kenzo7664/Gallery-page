2a693b43c121d357054b71993271ad00374d70d9
kenzo7664 <olaotanemmanuel1@gmail.com>
2021-10-04 10:52:17 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -20,13 +20,13 @@ function App() {
     <div className="container mx-auto " >
       <ImageSearch searchText = {(text)=> setTerm(text)}/>
       {!isLoading && images.length  === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1>  }
-      {/* { isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading ...</h1>: */}
+      { isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading ...</h1>:
       <div className="grid grid-cols-1 gap-4 lg:grid-cols-4 md:grid-cols-2" >
         {images.map(image => (
           <ImageCard key ={image.id} image={image} />
         ))
         }
-      </div>
+      </div>}
     </div>
   );
 }
